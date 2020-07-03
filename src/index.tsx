b24113341d50a1813cb4d7446cd6c06e7de108dc
phantom05 <monster2jy@gmail.com>
2020-07-03 13:00:33 +0900

    Update: set default typescript & react

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '~/components/App';
+// import Button from '~/components/common/Button';
+import _ from 'lodash';
+// import { img_apple } from '~/components/base/images';
 
-function index(props) {
-  return <div></div>;
+const abc = [1, 2, 3];
+
+const aaa = _.map(abc, i => i + 5);
+console.log(aaa, 'aaa');
+export interface HelloProps {
+  compiler: string;
+  framework: string;
 }
 
-export default index;
+export const Hello = (props: HelloProps) => (
+  <h1>
+    Hello from z{props.compiler} and {props.framework}! <App />
+    {/* <Button /> */}
+    {/* <img src={img_apple} alt="" /> */}
+  </h1>
+);
+
+ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      append: {
+        head: `<script src="//cdn.polyfill.io/v3/polyfill.min.js"></script>`,
+      },
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new LodashModuleReplacementPlugin(),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+  ],
+  module: {
+    rules: [
+      {
+        //   //babel이 있어야 일반 type 없는 js를 ts에 넣을 수 있음
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+
+      {
+        // ts-loader에다가 js확장자를 넣으면 안됨
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      { test: /\.html$/, use: 'html-loader' },
+      {
+        test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: 'file-loader',
+      },
+      { test: /\.(a?png|svg)$/, use: 'url-loader?limit=10000' },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      '~': path.resolve(__dirname, 'src/'),
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  devServer: {
+    port: 9995,
+    contentBase: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use 'contenthash' when hot reloading is enabled.
+    config.output.filename = '[name].[hash].js';
+  }
+
+  return config;
+};
