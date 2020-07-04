6ef0096619041cf488a52ff71920f8e1855d94de
phantom05 <monster2jy@gmail.com>
2020-07-04 11:06:07 +0900

    Add : main Readme.md

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# develop Frontend
+
+### ***server***
+
++ test server
+
+```nginx
+$ cd app
+$ npm i
+$ npm start
+```
+
+> https://localhost:5551
+
+
+
+### ***app***
+
+- react app
+
+```nginx
+$ cd app
+$ npm i
+$ npm start
+```
+
+> https://localhost:8800
--- app/src/Root.tsx
+++ app/src/Root.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from '~/components/App';
 // import store from 'store';
 // import { LastLocationProvider } from 'react-router-last-location';
