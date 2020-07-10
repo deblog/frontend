9999b9d59a4ca15d6c9cfeeb7b3314bd3a8cc1be
phantom05 <monster2jy@gmail.com>
2020-07-11 02:57:04 +0900

    Update: graphql 서버 디비에 붙혀보기

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -6,12 +6,6 @@ export const API_ADDRESS = ENV_MODE_DEV
   ? `http://127.0.0.1:4000/graphql`
   : `http://127.0.0.1:4000/graphql`;
 
-export const api_address = ENV_MODE_DEV ? `http://127.0.0.1:9999` : `http://127.0.0.1:9999`;
-export const lfw_address = ENV_MODE_DEV ? `http://localhost:13986` : `http://15.164.27.98:28180`;
-// NOTE: Electron Local Test
-// export const lfw_address = ENV_MODE_PROD ? `http://localhost:3000` : `http://15.164.27.98:28180`;
-// export const lfw_address = `http://15.164.27.98:28180`;
-export const lfw_bin_address = `http://15.164.27.98:28700`;
-export const test_server_address = `http://localhost:9999`;
-
-// http://15.164.27.98:3000
+export const API_BIN_ADDRESS = ENV_MODE_DEV
+  ? `http://127.0.0.1:4000/graphql`
+  : `http://127.0.0.1:4000/graphql`;
