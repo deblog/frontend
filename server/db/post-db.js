c09b352b56cbf3f5a5a762c1b09bd2ccad29768a
phantom05 <monster2jy@gmail.com>
2020-07-10 23:20:57 +0900

    Update: change to folder in pages

--- server/db/post-db.js
+++
@@ -1,7 +0,0 @@
-export const posts = [...Array(100).keys()].map(i => ({
-  id: i,
-  title: `Title ${i}`,
-  summary:
-    " has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  src: 'https://velopert.com/wp-content/uploads/2018/04/prettier.png',
-}));
--- server/db/user-db.js
+++
@@ -1,4 +0,0 @@
-export const users = [...Array(100).keys()].map(i => ({
-  id: i,
-  name: `User${i}`,
-}));
