28a025307019a6b3f27c66c313c08dfdbe582ec1
phantom05 <monster2jy@gmail.com>
2020-07-20 19:55:11 +0900

    업데이트:타입스크립트를 이용한 투두리스트

--- app/src/containers/todos/TodosContainer.tsx
+++ app/src/containers/todos/TodosContainer.tsx
@@ -1,50 +1,73 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import styled from '@emotion/styled';
+import cx from 'classnames';
 import { useImmer } from 'use-immer';
 import { color } from '~/styles/_utils';
-import { useCounter } from '~/components/base/hooks';
+import { useCounter, useTodos } from '~/components/base/hooks';
 import { increase, decrease, increaseBy } from '~/store/modules/counter';
+import { insertTodo, deleteTodo, toggleTodo } from '~/store/modules/base';
 import { useDispatch } from 'react-redux';
 import { withZeroNum } from '~/lib/utils';
-
-// import { useTodos } from '~/components/base/hooks';
 
 interface clicktypes {
   type: string;
   name?: string;
+  value?: number;
 }
 interface changeTypes {
   type: string;
-  value: string;
   name?: string;
+  value: string;
+  event?: object;
+}
+interface keyupTypes {
+  type: string;
+  name?: string;
+  value?: string;
+  event?: object;
+}
+interface submitTypes {
+  type: string;
+  name?: string;
+  value?: string;
+  event?: object;
 }
 
 const TodosContainerState = {
   title: 'Todos',
   todoInput: '',
 };
-function TodosContainer() {
+const TodosContainer = () => {
   const [values, setValues] = useImmer(TodosContainerState);
-  const counter = useCounter();
+  // const counter = useCounter();
   const dispatch = useDispatch();
+  const baseTodos = useTodos();
 
-  const handleChange = (config: changeTypes) => {
+  // NOTE: keyup event
+  const handleKeyup = (config: keyupTypes): void => {
+    // const { event} } = config;
+    // console.log(event);
+  };
+  // NOTE: change event
+  const handleChange = (config: changeTypes): void => {
     const { type, value } = config;
+    console.log(config);
     if (type === 'todo') {
       setValues(draft => {
         draft.todoInput = value;
       });
     }
   };
+  // NOTE: click event
   const handleClick = (config: clicktypes): void => {
-    const { type, name } = config;
+    const { type, name, value } = config;
     if (type === 'todo') {
-      const value = values.todoInput;
-      console.log(value);
-      setValues(draft => {
-        draft.todoInput = '';
-      });
-      console.log('done!');
+      if (name === 'delete') {
+        dispatch(deleteTodo({ id: value }));
+      }
+      if (name === 'toggle') {
+        dispatch(toggleTodo({ id: value }));
+      }
     }
     if (type === 'counter') {
       if (name === 'up') {
@@ -58,38 +81,86 @@ function TodosContainer() {
     }
   };
 
-  console.log(values);
+  // NOTE: submit event
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    setValues(draft => {
+      draft.todoInput = '';
+    });
+    dispatch(insertTodo({ title: values.todoInput }));
+  };
+
   return (
     <Styled.TodosContainer>
       <h2 className="title">{values.title}</h2>
       <div>
-        <input
-          type="text"
-          placeholder="Please enter your task."
-          className="todos__input"
-          onChange={e => handleChange({ type: 'todo', value: e.target.value })}
-          value={values.todoInput}
-        />
-        <button className="todos__btn" onClick={() => handleClick({ type: 'todo', name: 'todos' })}>
-          Done
-        </button>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Please enter your task."
+            className="todos__input"
+            onChange={e => handleChange({ type: 'todo', name: 'insert', value: e.target.value })}
+            value={values.todoInput}
+          />
+          <button type="submit" className="todos__btn" onClick={handleSubmit}>
+            Done
+          </button>
+        </form>
       </div>
+
+      {baseTodos.todos.map((item, idx) => {
+        console.log();
+        return (
+          <div key={idx} className="todo__item_rows">
+            <span
+              className={cx('todo__item_tx', { checked: item.toggle })}
+              onClick={() => handleClick({ type: 'todo', name: 'toggle', value: item.id })}
+            >
+              {item.title}
+            </span>
+            <button
+              className="todos__btn delete"
+              onClick={() => handleClick({ type: 'todo', name: 'delete', value: item.id })}
+            >
+              (X)
+            </button>
+          </div>
+        );
+      })}
+
+      {/* <Counter onClick={handleClick} count={counter.count} /> */}
+    </Styled.TodosContainer>
+  );
+};
+
+interface onClickProps {
+  type: string;
+  name: string;
+}
+interface typeCounterProps {
+  onClick?: (val: onClickProps) => void;
+  count?: number;
+}
+const Counter = (props: typeCounterProps) => {
+  const { onClick = () => {}, count = 0 } = props;
+  return (
+    <>
       <h2 className="title">Counter</h2>
       <div>
-        <button
-          className="todos__btn"
-          onClick={() => handleClick({ type: 'counter', name: 'down' })}
-        >
+        <button className="todos__btn" onClick={() => onClick({ type: 'counter', name: 'down' })}>
           -
         </button>
-        <span className="count">{withZeroNum(counter.count)}</span>
-        <button className="todos__btn" onClick={() => handleClick({ type: 'counter', name: 'up' })}>
+        <span className="count">{withZeroNum(count)}</span>
+        <button
+          className="todos__btn delete"
+          onClick={() => onClick({ type: 'counter', name: 'up' })}
+        >
           +
         </button>
       </div>
-    </Styled.TodosContainer>
+    </>
   );
-}
+};
 const Styled = {
   TodosContainer: styled.div`
     .title {
@@ -104,8 +175,26 @@ const Styled = {
       padding: 5px;
     }
     .todos__btn {
+      display: inline-block;
       cursor: pointer;
       padding: 5px 10px;
+
+      &.delete {
+        color: red;
+        background: white;
+        border: 1px solid #ececec;
+      }
+    }
+    .todo__item_tx {
+      display: inline-block;
+      width: 200px;
+      cursor: pointer;
+      &:hover {
+        background: #ececec;
+      }
+      &.checked {
+        text-decoration: line-through;
+      }
     }
   `,
 };
